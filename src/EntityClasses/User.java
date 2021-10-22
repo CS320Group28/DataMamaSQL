696aea9044effa5027247dc7faf3e363bc33aaa1
zpg1630 <zpg1630@g.rit.edu>
2021-10-21 23:41:53 -0400

    Basic User class implemented.-Zach

--- src/EntityClasses/User.java
+++ src/EntityClasses/User.java
@@ -1,6 +1,52 @@
 package EntityClasses;
 
+import java.time.LocalDateTime;
+
 public class User implements EntityType<User>{
+    private String username;
+    private String password;
+    private LocalDateTime creationDate;
+    private LocalDateTime lastAccessDate;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+        creationDate = LocalDateTime.now();
+        lastAccessDate = creationDate;
+    }
+
+    public void setUserName(String username){
+        this.username = username;
+    }
+
+    public String getUserName(){
+        return this.username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate){
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getCreationDate(){
+        return this.creationDate;
+    }
+
+    public void setLastAccessDate(LocalDateTime lastAccessDate){
+        this.lastAccessDate = lastAccessDate;
+    }
+
+    public LocalDateTime getLastAccessDate(){
+        return this.lastAccessDate;
+    }
+
     @Override
     public boolean InsertEntity(User entity) {
         return false;
