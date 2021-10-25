c173f9e7ab16504717be6aefd564b78d27f435cc
jrcoa <jrccasale@gmail.com>
2021-10-24 21:47:21 -0400

    removing unneeded directory

--- src/com/ConsoleApp/CommandClasses/CreateAccount.java
+++ src/com/ConsoleApp/CommandClasses/CreateAccount.java
@@ -1,13 +1,17 @@
-package ConsoleApp.CommandClasses;
+package com.ConsoleApp.CommandClasses;
+
 
 import java.util.Scanner;
 
 public class CreateAccount {
     private static final Scanner in = new Scanner(System.in);
 
-    public void CreateAccountCLI(){
+    public void CreateAccountCLI(DBInterface db){
         System.out.println("Enter a username: ");
         String username = in.next();
+        System.out.println("Enter a password: ");
+        String password = in.next();
+        
         
     }
     
